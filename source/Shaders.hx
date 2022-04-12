1ef4672ba275abca2132158b4ff3e5108333d3a3
jonnycat <82131956+McJonnycat@users.noreply.github.com>
2022-04-11 23:19:44 -0700

    fixed it

--- source/Shaders.hx
+++ source/Shaders.hx
@@ -133,8 +133,8 @@ class TiltshiftEffect extends Effect{
 	public var shader:Tiltshift;
 	public function new (blurAmount:Float, center:Float){
 		shader = new Tiltshift();
-		shader.data.bluramount.value = [blurAmount];
-		shader.data.center.value = [center];
+		shader.data.bluramount.value = [1.0];
+		shader.data.center.value = [1.0];
 	}
 	
 	
@@ -181,8 +181,8 @@ class Tiltshift extends FlxShader
 		 
 		// I am hardcoding the constants like a jerk
 			
-		uniform float bluramount  = 1.0;
-		uniform float center      = 1.0;
+		uniform float bluramount;
+		uniform float center;
 		const float stepSize    = 0.004;
 		const float steps       = 3.0;
 		 
